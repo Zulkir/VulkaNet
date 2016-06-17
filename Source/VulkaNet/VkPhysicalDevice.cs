d61a6eabadb6f2641ebdd1ce3eff3edb50ce1492
Daniil Rodin <furidan@gmail.com>
2016-06-17 17:04:10 +0300

    Queue families query

--- Source/VulkaNet/VkPhysicalDevice.cs
+++ Source/VulkaNet/VkPhysicalDevice.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using VulkaNet.InternalHelpers;
 
 namespace VulkaNet
@@ -6,6 +8,7 @@ namespace VulkaNet
     public interface IVkPhysicalDevice : IVkHandledObject
     {
         IVkPhysicalDeviceProperties Properties { get; }
+        IReadOnlyList<IVkQueueFamilyProperties> QueueFamilyProperties { get; }
     }
 
     public unsafe class VkPhysicalDevice : IVkPhysicalDevice
@@ -13,12 +16,14 @@ namespace VulkaNet
         public IntPtr Handle { get; }
         public DirectFunctions Direct { get; }
         public IVkPhysicalDeviceProperties Properties { get; }
+        public IReadOnlyList<IVkQueueFamilyProperties> QueueFamilyProperties { get; }
 
         public VkPhysicalDevice(IVkInstance instance, IntPtr handle)
         {
             Handle = handle;
             Direct = new DirectFunctions(instance);
             Properties = GetPhysicalDeviceProperties();
+            QueueFamilyProperties = GetPhysicalDeviceQueueFamilyProperties();
         }
 
         public class DirectFunctions
@@ -28,10 +33,18 @@ namespace VulkaNet
                 IntPtr physicalDevice,
                 VkPhysicalDeviceProperties.Raw* pProperties);
 
+            public GetPhysicalDeviceQueueFamilyPropertiesDelegate GetPhysicalDeviceQueueFamilyProperties { get; }
+            public delegate void GetPhysicalDeviceQueueFamilyPropertiesDelegate(
+                IntPtr physicalDevice,
+                int* pQueueFamilyPropertyCount,
+                VkQueueFamilyProperties.Raw* pQueueFamilyProperties);
+
             public DirectFunctions(IVkInstance instance)
             {
                 GetPhysicalDeviceProperties =
                     VkHelpers.GetDelegate<GetPhysicalDevicePropertiesDelegate>(instance, "vkGetPhysicalDeviceProperties");
+                GetPhysicalDeviceQueueFamilyProperties =
+                    VkHelpers.GetDelegate<GetPhysicalDeviceQueueFamilyPropertiesDelegate>(instance, "vkGetPhysicalDeviceQueueFamilyProperties");
             }
         }
 
@@ -40,6 +53,18 @@ namespace VulkaNet
             VkPhysicalDeviceProperties.Raw raw;
             Direct.GetPhysicalDeviceProperties(Handle, &raw);
             return new VkPhysicalDeviceProperties(&raw);
+        }
+
+        private IReadOnlyList<IVkQueueFamilyProperties> GetPhysicalDeviceQueueFamilyProperties()
+        {
+            int count;
+            Direct.GetPhysicalDeviceQueueFamilyProperties(Handle, &count, (VkQueueFamilyProperties.Raw*)0);
+            var rawArray = new VkQueueFamilyProperties.Raw[count];
+            fixed (VkQueueFamilyProperties.Raw* pRawArray = rawArray)
+            {
+                Direct.GetPhysicalDeviceQueueFamilyProperties(Handle, &count, pRawArray);
+                return rawArray.Select(x => new VkQueueFamilyProperties(&x)).ToArray();
+            }
         }
     }
 }
